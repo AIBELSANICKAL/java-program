18a961b42b0c68272a73ba420082b5320fb8869f
AIBEL S ANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2025-07-18 09:20:28 +0530

    Create neven.java

---
+++ neven.java
@@ -0,0 +1,16 @@
+package psn;
+import java.util.Scanner;
+public class Neven {
+    public static void  main(String[] args) {
+    	Scanner sc= new Scanner(System.in);
+    	System.out.println("Enter the number:");
+    	int number=sc.nextInt();
+    	int i;
+    	for(i=0;i<number;i+=2) {
+    		System.out.println(i); 
+    		
+    	}
+    
+    	
+    }
+}
