8afa2e859714dbcd71838ae03f960d732294be76
AIBEL S ANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2025-07-18 10:12:10 +0530

    Create book.java

---
+++ book.java
@@ -0,0 +1,27 @@
+package psn;
+   class Book{
+   String Title;
+   String Author;
+   double price;
+    Book(){
+	   Title="unknown";
+	   Author="Not assingned";
+	   price= 0.0;
+   }
+   public void displayDetails() {
+        System.out.println("Title:\t"+Title);
+        System.out.println("Author:\t"+Author);
+        System.out.println("price:\t"+price);
+          	   
+      }
+}
+      
+    	  public class constructorDemo() {
+    		  public static void main(string [] args) {
+    			  Book book=new books();
+    			  book.displayDetails();
+    			  
+    		  }
+    		  
+    	  }
+/// incomplete
