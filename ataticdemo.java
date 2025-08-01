5802a5423a9ea69b32163f346a87f37fa0754c72
AIBEL S ANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2025-08-01 09:08:31 +0530

    Create ataticdemo.java

---
+++ ataticdemo.java
@@ -0,0 +1,24 @@
+package package_1;
+
+class Student{
+	String name;
+	static int studentCount = 0;
+	
+	Student(String name){
+		this.name = name;
+		studentCount++;
+	}
+	public void printDetails() {
+		System.out.println(name);
+		System.out.println(studentCount);
+	}
+}
+
+public class Static_Demo {
+	public static void main(String []args) {
+		Student student1 = new Student("Stud1");
+		student1.printDetails();
+		Student student2 = new Student("Stud2");
+		student2.printDetails();
+	}
+}
